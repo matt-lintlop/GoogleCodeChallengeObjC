dd56bdeb7502df56fba39330111e555f7c066f20
Matt Lintlop <matt_lintlop@icloud.com>
2019-07-02 09:54:18 -0700

    Implemented canRotateStrings(char* string1, char*string2)

--- GoogleCodeChallengeObjC/StringRotatorCPP.cpp
+++ GoogleCodeChallengeObjC/StringRotatorCPP.cpp
@@ -16,5 +16,31 @@ vector<char*>* StringRotatorCPP::findFirstCommonRotatedStrings(vector<char*>* in
 }
 
 bool StringRotatorCPP::canRotateStrings(char* string1, char*string2) {
-    return false;
+    if ((string1 == NULL) || (string2 == NULL)) {
+        return false;
+    }
+    size_t string1Length = strlen(string1);
+    size_t string2Length = strlen(string2);
+    if (string1Length != string2Length) {
+        return false;
+    }
+    int firstCharRotateCount = 0;
+    for (int index = 0; index < string1Length; index++) {
+        int charRotateCount = string2[index] - string1[index];
+        while (charRotateCount < 0) {
+            charRotateCount += 26;
+        }
+        while (charRotateCount > 25) {
+            charRotateCount -= 26;
+        }
+        if (index == 0) {
+            firstCharRotateCount = charRotateCount;
+        }
+        else {
+            if (charRotateCount != firstCharRotateCount) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
